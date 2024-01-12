b90132c376ec29617a3f2597ed717df147781a67
hoaggggggg <156327794+hoaggggggg@users.noreply.github.com>
2024-01-12 10:07:37 +0700

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -4,5 +4,6 @@ cd ~
 sudo apt install git aria2 -y
 git clone https://gitlab.com/OrangeFox/misc/scripts
 cd scripts
+export OLDPWD=$(pwd)
 sudo bash setup/android_build_env.sh
 sudo bash setup/install_android_sdk.sh
